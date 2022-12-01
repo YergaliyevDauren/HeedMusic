7a2d660fb37d99094e83fb660b166051da4f5686
Iskandar Rassulov <iskanderesz@gmail.com>
2022-12-01 23:46:41 +0600

    connect to firebase
    Connecting a project to firebase

--- app/src/main/java/din/heed_music/HomeFragment.kt
+++ app/src/main/java/din/heed_music/HomeFragment.kt
@@ -1,7 +1,6 @@
 package din.heed_music
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -10,7 +9,7 @@ import androidx.recyclerview.widget.RecyclerView
 import din.adapter.CarouselAdapter
 import din.data.Datasource
 
-class HomeFragment : Fragment() {
+class HomeFragment : androidx.fragment.app.Fragment() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
---
+++ app/src/main/java/din/heed_music/LoginPage.kt
@@ -0,0 +1,4 @@
+package din.heed_music
+
+class LoginPage {
+}
